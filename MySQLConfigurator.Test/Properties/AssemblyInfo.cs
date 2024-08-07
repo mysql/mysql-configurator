c2c92e25f792ed46b9f6d8fe8315d695bf4be110
Jose Ramirez <jose.m.ramirez@oracle.com>
2024-08-07 14:03:01 -0600

    WL16434 - Inject version into Configurator building process
    - Removed version entries from AssemblyInfo files since we want to assign the version dynamically (Configurator/Properties/AssemblyInfo.cs, MySQLConfigurator.Test/Properties/AssemblyInfo.cs).
    - Added a Version property which will define the default version if no version is provided when building with MSBuild or when building using Visual Studio. Added a BeforeBuild target that allows to set the Version with the value provided when building with MSBuild (Configurator/MySQLConfigurator.csproj, MySQLConfigurator.Test/MySQLConfigurator.Test.csproj).
    
    Change-Id: Ie7177cdfd39d84f4da7bd157e6914a794e1b1b8d

--- MySQLConfigurator.Test/Properties/AssemblyInfo.cs
+++ MySQLConfigurator.Test/Properties/AssemblyInfo.cs
@@ -33,7 +33,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("5304b25d-746b-465d-b866-48606d9b6f68")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.1.0.0")]
-[assembly: AssemblyFileVersion("9.1.0.0")]
